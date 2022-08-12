635a505204c01d4d86887e0a94958dd6db908816
Humberto :) <humbertomurad@uol.com.br>
2022-08-11 23:04:41 -0300

    Criação da pasta DEV, contendo as camadas da aplicação comduas classes simples somente para se testar a exibição

---
+++ IFSPPJIG05DEV/CONTROLLER/Inicializador.java
@@ -0,0 +1,9 @@
+package IFSPPJIG05DEV.CONTROLLER;
+
+public class Inicializador
+{
+    public static void main(String[] args) 
+    {
+        IFSPPJIG05DEV.VIEW.Exibicao.HiSystem();    
+    }
+}
---
+++ IFSPPJIG05DEV/VIEW/Exibicao.java
@@ -0,0 +1,13 @@
+package IFSPPJIG05DEV.VIEW;
+
+import javax.swing.JOptionPane;
+
+public class Exibicao 
+{
+
+    public static void HiSystem()
+    {
+        JOptionPane.showMessageDialog(null, "Hi System!");
+    }
+    
+}
