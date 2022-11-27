6751e1066017b4251ecea8dbbc585d7e9b6557e5
Tavares194 <gabriel.tavares.1904@gmail.com>
2022-11-27 14:00:55 -0300

    Update: inserts em tabelas e criação de views

--- IFSPPJIG05DEV/database/database_GHYM.sql
+++ IFSPPJIG05DEV/database/database_GHYM.sql
@@ -34,7 +34,8 @@ insert into Marca values
 (23, 'ADATA'),
 (24, 'TeamGroup'),
 (25, 'Kingston'),
-(26, 'Husky Gaming');
+(26, 'Husky Gaming'),
+(27, 'Samsung');
 
 -- NOME DA TABELA NÃO PODE SER 'CPU', POIS É UMA PALAVRA RESERVADA
 create table Processador(
@@ -185,7 +186,42 @@ insert into GPU (cod_gpu, id_marca, nome, clock, mark, VRAM, TDP, preco) values
 (12, 11, 'Geforce GT 610', 810, 302, 2, 20, 229.99),
 (13, 11, 'Radeon R5 220', 1066, 135, 1, 20, 259.99),
 (14, 20, 'Geforce GT 710', 800, 637, 2, 25, 399.99),
-(15, 7, 'Geforce GT 1030', 1250, 2539, 2, 30, 899.99);
+(15, 7, 'Geforce GT 1030', 1250, 2539, 2, 30, 899.99),
+(16, 7, 'Radeon RX 6800 XT', 1250, 2539, 2, 30, 899.99),
+(17, 10, 'Titan RTX', 1770, 19897, 24, 280, 10999.99),
+(18, 4, 'GeForce RTX 2080 Super', 1815, 19546, 8, 250, 2499.99),
+(19, 4, 'GeForce RTX 2080', 1710, 18748, 8, 215, 2299.99),
+(20, 2, 'Radeon VII', 1750, 16837, 16, 295, 1199.99),
+(21, 5, 'GeForce RTX 3080 Ti', 1667, 27283, 12, 350, 4899.99),
+(22, 4, 'GeForce RTX 3080', 1710, 25231, 10, 320, 4299.99),
+(23, 4, 'GeForce RTX 3070', 1725, 22323, 8, 220, 3599.99),
+(24, 2, 'Titan V', 1455, 19773, 12, 250, 3599.99),
+(25, 2, 'Titan Xp', 1582, 19155, 12, 250, 2899.99),
+(26, 5, 'GeForce RTX 2070', 1620, 16125, 8, 175, 2199.99),
+(27, 13, 'Radeon RX 5600 XT', 1750, 13833, 6, 150, 899.99),
+(28, 14, 'GeForce GTX 1080', 1733, 15352, 8, 180, 1599.99),
+(29, 14, 'GeForce GTX 1070 Ti', 1683, 14645, 8, 180, 1299.99),
+(30, 10, 'GeForce GTX 1070', 1683, 13507, 8, 150, 1099.99),
+(31, 3, 'GTX Titan X', 1075, 13177, 12, 250, 1499.99),
+(32, 14, 'GeForce GTX 980 Ti', 1075, 13856, 6, 250, 1699.99),
+(33, 15, 'Radeon R9 Fury X', 1050, 10078, 4, 275, 2099.99),
+(34, 6, 'Radeon RX 590', 1545, 9657, 8, 175, 1399.99),
+(35, 15, 'Radeon RX 5500 XT', 1845, 9196, 8, 130, 1499.99),
+(36, 6, 'Radeon RX 580', 1340, 8923, 8, 185, 1199.99),
+(37, 4, 'GeForce GTX 1650 Super', 1725, 10077, 4, 125, 1299.99),
+(38, 2, 'GeForce GTX 1660', 1785, 9048, 6, 120, 1399.99),
+(39, 4, 'GeForce GTX 1060', 1708, 10069, 6, 120, 1499.99),
+(40, 14, 'GeForce GTX 980', 1216, 11221, 4, 165, 1199.99),
+(41, 11, 'Radeon RX 570', 1244, 8100, 8, 120, 1199.99),
+(42, 13, 'GeForce GTX 970', 1178, 9647, 4, 145, 699.99),
+(43, 5, 'Geforce GTX 1650', 1665, 7827, 4, 75, 799.99),
+(44, 6, 'GeForce GTX 1050 Ti', 1392, 6298, 4, 75, 699.99),
+(45, 16, 'Radeon RX 560', 1275, 3613, 4, 75, 999.99),
+(46, 14, 'GeForce GT 1030', 1670, 2518, 4, 30, 399.99),
+(47, 14, 'GeForce GTX 1050', 1392, 5054, 2, 75, 899.99),
+(48, 5, 'GeForce RTX 4090', 2520, 39062, 24, 450, 15899.99),
+(49, 5, 'GeForce RTX 4080', 2505, 33652, 16, 320, 9899.99),
+(50, 7, 'Radeon RX 6950 XT', 2324, 28659, 16, 335, 6799.99);
 
 create table RAM(
 cod_ram int NOT NULL,
@@ -214,7 +250,22 @@ insert into RAM values
 (12, 22, 'Ballistix White', 3000, 8, 'DDR4', 352.99),
 (13, 22, 'Ballistix Black', 3200, 8, 'DDR4', 352.99),
 (14, 25, 'Fury Beast', 3000, 8, 'DDR4', 411.99),
-(15, 25, 'Fury Beast Black', 2666, 8, 'DDR4', 295.99);
+(15, 25, 'Fury Beast Black', 2666, 8, 'DDR4', 295.99),
+(16, 27, 'Samsung Memory', 1600, 4, 'DDR3', 89.99),
+(17, 17, 'Multi CL11', 1600, 6, 'DDR3', 114.99),
+(18, 25, 'KVR16 Kingston', 1600, 4, 'DDR3', 84.99),
+(19, 21, 'Corsair C11', 1600, 6, 'DDR3', 129.99),
+(20, 21, 'Corsair CL15', 2133, 4, 'DDR4', 125.99),
+(21, 25, 'Kingston CL19', 2666, 4, 'DDR4', 144.99),
+(22, 25, 'Fury CL17', 2400, 4, 'DDR4', 295.99),
+(23, 25, 'Kingston Fury CL16', 2666, 4, 'DDR4', 166.99),
+(24, 26, 'Gaming Avalanche', 3200, 16, 'DDR4', 269.99),
+(25, 9, 'XPG Gammix D10', 3200, 16, 'DDR4', 359.99),
+(26, 25, 'Kingston Fury Beast RGB', 3200, 16, 'DDR4', 409.99),
+(27, 9, 'XPG Gammix D10 Black', 3200, 16, 'DDR4', 470.99),
+(28, 21, 'Vengeance LPX CL16', 2400, 16, 'DDR4', 339.99),
+(29, 22, 'Crucial Basics 6GB', 2666, 6, 'DDR4', 114.99),
+(30, 21, 'Corsair CL15 6GB', 2133, 6, 'DDR4', 125.99);
 
 create table Tipo(
 cod_tipo int NOT NULL,
@@ -253,7 +304,12 @@ insert into RecomendacaoCpu values
 (7, 4, 4, 3.10, 3.50),
 (8, 2, 4, 2.10, 3.30),
 (9, 2, 4, 2.60, 3.10),
-(10, 4, 4, 3.10, 3.70);
+(10, 4, 4, 3.10, 3.70),
+(11, 4, 12, 2.90, 3.50),
+(12, 4, 6, 2.20, 3.00),
+(13, 4, 8, 3.10, 3.70),
+(14, 4, 8, 2.20, 3.20),
+(15, 4, 12, 3.10, 3.70);
 
 create table RecomendacaoGpu(
 cod_recgpu int NOT NULL,
@@ -288,7 +344,12 @@ insert into RecomendacaoRam values
 (2, 2, 4),
 (3, 6, 8),
 (4, 4, 8),
-(5, 2, 4);
+(5, 2, 4),
+(6, 8, 16),
+(7, 4, 4),
+(8, 8, 8),
+(9, 16, 16),
+(10, 16, 32);
 
 create table Software(
 cod_soft int NOT NULL,
@@ -306,26 +367,164 @@ FOREIGN KEY (id_tipo) REFERENCES Tipo(cod_tipo)
 );
 
 insert into Software (cod_soft, id_recomendacaoram, id_recomendacaogpu, id_recomendacaocpu, id_tipo, nome_soft) values
-(1, 1, 2, 7, 1, 'God of War'),
-(2, 1, 5, 1, 2, 'Adobe Photoshop'),
-(3, 1, 7, 8, 1, 'Counter-Strike Global Offensive'),
-(4, 3, 1, 2, 1, 'Ghost of Tsushima'),
+(1, 6, 2, 10, 1, 'God of War'),
+(2, 1, 5, 12, 2, 'Adobe Photoshop'),
+(3, 1, 7, 8, 1, 'CS GO'),
+(4, 3, 6, 14, 1, 'Ghost of Tsushima'),
 (5, 1, 1, 4, 1, 'GTA V'),
-(6, 1, 2, 5, 1, 'Marvels Spider-Man Miles Morales'),
-(7, 1, 9, 5, 1, 'Marvels Avenger'),
-(8, 1, 6, 3, 1, 'Dark Souls III'),
+(6, 6, 9, 14, 1, 'Marvels Spider-Man Miles Morales'),
+(7, 6, 9, 14, 1, 'Marvels Avenger'),
+(8, 1, 9, 2, 1, 'Dark Souls III'),
 (9, 1, 3, 9, 1, 'Jurassic World Evolution 2'),
-(10, 1, 3, 1, 1, 'Need for Speed Heat'),
-(11, 1, 5, 8, 1, 'Fortnite'),
-(12, 1, 3, 10, 1, 'Genshin Impact'),
-(13, 1, 8, 2, 3, 'Adobe Premiere Pro'),
-(14, 1, 3, 1, 7, 'Wireshark'),
-(15, 1, 3, 1, 4, 'MySQL'),
-(16, 1, 3, 2, 4, 'Visual Studio'),
-(17, 1, 3, 1, 4, 'Eclipse'),
-(18, 1, 3, 1, 8, 'Chrome'),
-(19, 1, 10, 1, 7, 'Geforce Experience'),
-(20, 1, 3, 1, 6, 'Office 365');
+(10, 1, 10, 9, 1, 'Need for Speed Heat'),
+(11, 1, 3, 5, 1, 'Fortnite'),
+(12, 1, 1, 12, 1, 'Genshin Impact'),
+(13, 1, 2, 10, 3, 'Adobe Premiere Pro'),
+(14, 1, 5, 1, 7, 'Wireshark'),
+(15, 1, 5, 1, 4, 'MySQL'),
+(16, 1, 5, 9, 4, 'Visual Studio'),
+(17, 1, 5, 9, 4, 'Eclipse'),
+(18, 1, 5, 6, 8, 'Chrome'),
+(19, 1, 5, 1, 7, 'Geforce Experience'),
+(20, 1, 5, 8, 6, 'Office'),
+(21, 6, 7, 3, 1, 'Hot Wheels UNLEASHED'),
+(22, 3, 4, 7, 1, 'Apex Legends'),
+(23, 8, 3, 7, 1, 'Assassins Creed 3'),
+(24, 2, 3, 6, 1, 'Assassins Creed 4 Black Flag'),
+(25, 2, 3, 6, 1, 'Assassins Creed Rogue'),
+(26, 3, 3, 6, 1, 'Assassins Creed Unity'),
+(27, 3, 3, 6, 1, 'Assassins Creed Syndicate'),
+(28, 3, 4, 4, 1, 'Assassins Creed Origins'),
+(29, 8, 4, 2, 1, 'Assassins Creed Odyssey'),
+(30, 1, 3, 4, 1, 'Overcooked 2'),
+(31, 3, 4, 6, 1, 'Dark Souls Remastered'),
+(32, 2, 7, 5, 1, 'Brawlhalla'),
+(33, 6, 9, 10, 1, 'Call of Duty Black Ops 4'),
+(34, 6, 9, 10, 1, 'Call of Duty Warzone'),
+(35, 6, 9, 10, 1, 'Call of Duty Black Ops Cold War'),
+(36, 1, 10, 9, 1, 'Need for Speed Rivals'),
+(37, 8, 10, 9, 1, 'Need for Speed Hot Pursuit'),
+(38, 3, 10, 9, 1, 'Need for Speed Payback'),
+(39, 3, 10, 9, 1, 'Need for Speed 2015'),
+(40, 2, 10, 9, 1, 'Need for Speed Most Wanted'),
+(41, 1, 10, 9, 1, 'Hollow Knight'),
+(42, 3, 3, 9, 1, 'Disgaea 5'),
+(43, 1, 6, 6, 1, 'Rocket League'),
+(44, 6, 1, 7, 1, 'Crash Bandicoot 4'),
+(45, 8, 1, 7, 1, 'Stray'),
+(46, 8, 1, 4, 1, 'Mortal Kombat 11'),
+(47, 5, 8, 5, 1, 'Dead Cells'),
+(48, 6, 8, 5, 1, 'Mortal Shell'),
+(49, 8, 6, 12, 1, 'Crash Bandicoot Remake'),
+(50, 8, 1, 12, 1, 'FIFA 22'),
+(51, 6, 1, 12, 1, 'FIFA 23'),
+(52, 2, 8, 5, 1, 'The Survivalists'),
+(53, 6, 1, 3, 1, 'A Plague Tale Innocence'),
+(54, 6, 6, 4, 1, 'STAR WARS Squadrons'),
+(55, 6, 9, 14, 1, 'Horizon Zero Dawn'),
+(56, 6, 6, 14, 1, 'Days Gone'),
+(57, 1, 1, 3, 1, 'Subnautica'),
+(58, 8, 1, 3, 1, 'Subnautica Below Zero'),
+(59, 2, 3, 6, 1, 'LEGO Movie'),
+(60, 7, 3, 6, 1, 'LEGO Movie 2'),
+(61, 6, 10, 3, 1, 'Control'),
+(62, 1, 7, 3, 1, 'Cat Quest'),
+(63, 1, 7, 3, 1, 'Ni no Kuni Wrath of the White Witch'),
+(64, 1, 6, 7, 1, 'PAYDAY 2'),
+(65, 8, 10, 9, 1, 'Maneater'),
+(66, 1, 7, 9, 1, 'SUPERHOT'),
+(67, 9, 2, 13, 1, 'Elden Ring'),
+(68, 8, 1, 6, 1, 'Tom Clancy The Division 2'),
+(69, 6, 3, 6, 1, 'The Surge 2'),
+(70, 2, 3, 4, 1, 'Dark Souls 2'),
+(71, 1, 3, 3, 1, 'Resident Evil 1 Remake'),
+(72, 8, 6, 6, 1, 'Resident Evil 2 Remake'),
+(73, 8, 6, 6, 1, 'Resident Evil 3 Remake'),
+(74, 6, 9, 10, 1, 'Resident Evil 4 Remake'),
+(75, 8, 6, 6, 1, 'Resident Evil 4'),
+(76, 1, 1, 9, 1, 'Resident Evil 5'),
+(77, 8, 6, 3, 1, 'Resident Evil 6'),
+(78, 6, 4, 7, 1, 'Resident Evil 7'),
+(79, 6, 9, 4, 1, 'Resident Evil Village'),
+(80, 6, 9, 10, 1, 'Resident Evil Reverse'),
+(81, 8, 3, 9, 1, 'Resident Evil Revelations 1'),
+(82, 8, 1, 6, 1, 'Resident Evil Revelations 2'),
+(83, 2, 8, 5, 1, 'Steam'),
+(84, 2, 5, 5, 5, 'Discord'),
+(85, 2, 8, 5, 6, 'Adobe Acrobat');
+
+-- PROCURAMOS SITUAÇÕES DE FUNÇÃO PARA O PROJETO, MAS NÃO ENCONTRAMOS
+-- POR ISSO, CRIAMOS A FUNÇÃO DO NOSSO SLUG, CASO O NOSSO PROJETO SEJA SELECIONADO PARA O DESENVOLVIMENTO WEB 
+DELIMITER $$
+CREATE FUNCTION slug_pags(url text)
+    RETURNS TEXT DETERMINISTIC
+    BEGIN
+        DECLARE slug TEXT;
+        SET slug = TRIM(url);
+        SET slug = LOWER(slug);
+        SET slug = REPLACE(slug, ' ', '-');
+        SET slug = REPLACE(slug, '--', '-');
+        SET slug = REPLACE(slug, '_', '-');
+        SET slug = REPLACE(slug, ':', '-');
+        SET slug = REPLACE(slug, ':', '-');
+        SET slug = REPLACE(slug, ';', '-');
+        SET slug = REPLACE(slug, '|', '-');
+        SET slug = REPLACE(slug, ',', '');
+        SET slug = REPLACE(slug, '~', '');
+        SET slug = REPLACE(slug,  '^', '');
+        SET slug = REPLACE(slug, '´', '');
+        SET slug = REPLACE(slug, '`', '');
+        SET slug = REPLACE(slug, '=', '');
+        SET slug = REPLACE(slug, '+', '');
+        SET slug = REPLACE(slug, '', '');
+        SET slug = REPLACE(slug, '@', '');
+        SET slug = REPLACE(slug, '<', '');
+        SET slug = REPLACE(slug, '>', '');
+        SET slug = REPLACE(slug, '#', '');
+        SET slug = REPLACE(slug, '$', '');
+        SET slug = REPLACE(slug, '¨', '');
+        SET slug = REPLACE(slug, '&', '');
+        SET slug = REPLACE(slug, '*', '');
+        SET slug = REPLACE(slug, '(', '');
+        SET slug = REPLACE(slug, ')', '');
+        SET slug = REPLACE(slug, '[', '');
+        SET slug = REPLACE(slug, ']', '');
+        SET slug = REPLACE(slug, '{', '');
+        SET slug = REPLACE(slug, '}', '');
+        SET slug = REPLACE(slug, '°', '');
+        SET slug = REPLACE(slug, '/', '');
+        SET slug = REPLACE(slug, '\\', '');
+        SET slug = REPLACE(slug, '\'', '');
+        SET slug = REPLACE(slug, '?', '');
+        SET slug = REPLACE(slug, '!', '');
+        SET slug = REPLACE(slug, 'ã', 'a');
+        SET slug = REPLACE(slug, 'á', 'a');
+        SET slug = REPLACE(slug, 'â', 'a');
+        SET slug = REPLACE(slug, 'à', 'a');
+        SET slug = REPLACE(slug, 'ç', 'c');
+        SET slug = REPLACE(slug, 'ñ', 'n');
+        SET slug = REPLACE(slug, 'é', 'e');
+        SET slug = REPLACE(slug, 'ê', 'e');
+        SET slug = REPLACE(slug, 'é', 'e');
+        SET slug = REPLACE(slug, 'ẽ', 'e');
+        SET slug = REPLACE(slug, 'í', 'i');
+        SET slug = REPLACE(slug, 'ì', 'i');
+        SET slug = REPLACE(slug, 'î', 'i');
+        SET slug = REPLACE(slug, 'ĩ', 'i');
+        SET slug = REPLACE(slug, 'ó', 'o');
+        SET slug = REPLACE(slug, 'ò', 'o');
+        SET slug = REPLACE(slug, 'ô', 'o');
+        SET slug = REPLACE(slug, 'õ', 'o');
+        SET slug = REPLACE(slug, 'ú', 'u');
+        SET slug = REPLACE(slug, 'ù', 'u');
+        SET slug = REPLACE(slug, 'û', 'u');
+        SET slug = REPLACE(slug, 'ũ', 'u');
+        RETURN slug;
+    END
+    $$
+    
+-- SLUG
+SELECT slug_pags("Recomendação de configuração para softwares");
 
 -- EXEMPLO DE COMPARAÇÃO DE CPU
 SELECT * FROM Processador WHERE cod_cpu = 1;
@@ -334,6 +533,30 @@ SELECT * FROM Processador WHERE cod_cpu = 2;
 -- EXEMPLO DE COMPARAÇÃO DE GPU
 SELECT * FROM GPU WHERE cod_gpu = 1;
 SELECT * FROM GPU WHERE cod_gpu = 2;
+
+-- LISTA DE PROCESSADORES POR ORDEM DE FORÇA DE PROCESSAMENTO (MENOR PARA O MAIOR)
+CREATE OR REPLACE VIEW lista_cpu AS
+SELECT p.cod_cpu AS id, p.nome, m.nome AS marca, p.clock, p.turbo, p.cores, p.threads, p.mark, p.TDP AS TDP, p.arquitetura, p.preco 
+FROM Processador p 
+JOIN Marca m 
+ON p.id_marca = m.cod_marca
+ORDER BY mark ASC;
+
+-- LISTA DE MEMORIAS RAM POR ORDEM DE FORÇA DE DESEMPENHO (MENOR PARA O MAIOR)
+CREATE OR REPLACE VIEW lista_ram AS
+SELECT r.cod_ram AS id, r.nome, m.nome AS marca, r.frequencia, r.capacidade, r.tipo, r.preco 
+FROM Ram r
+JOIN Marca m 
+ON r.id_marca = m.cod_marca
+ORDER BY capacidade ASC;
+
+-- LISTA DE GPU POR ORDEM DE FORÇA DE PROCESSAMENTO GRÁFICO (MENOR PARA O MAIOR)
+CREATE OR REPLACE VIEW lista_gpu AS
+SELECT g.cod_gpu AS id, g.nome, m.nome AS marca, g.clock, g.VRAM, g.mark, g.TDP AS TDP, g.preco 
+FROM Gpu g
+JOIN Marca m 
+ON g.id_marca = m.cod_marca
+ORDER BY mark ASC;
 
 -- TELA DE RECOMENDAÇÃO DE UM SOFTWARE ESPECIFICO
 CREATE OR REPLACE VIEW requisitos_soft AS
@@ -397,8 +620,17 @@ INNER JOIN Marca m
 ON cod_marca = id_marca
 ORDER BY vram ASC;
 
+-- LISTA DE PROCESSADORES
+SELECT * FROM lista_cpu;
+
+-- LISTA DE RAM
+SELECT * FROM lista_ram;
+
+-- LISTA DE GPU
+SELECT * FROM lista_gpu;
+
 -- EXEMPLOS PARA USO NO SOFTWARE JAVA
-SELECT * FROM rec_soft
+SELECT * FROM requisitos_soft
 WHERE cod_soft IN (1);
 
 SELECT * FROM rec_ram
